9322f92ce04c73f57fe77baf6d876b2849626354
ccksam <chanyuksingsam@gmail.com>
2022-09-19 16:53:45 +0800

    edit css and remove Result component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import Result from "./components/Result";
 import Panel from "./components/Panel";
 import "./App.css";
 
 export default function App() {
 	return (
 		<div className="App">
-			<Result />
 			<Panel />
 		</div>
 	);
--- src/components/Panel/index.jsx
+++ src/components/Panel/index.jsx
@@ -4,27 +4,28 @@ import "./index.css";
 export default function Panel() {
 	return (
 		<div className="panel-container">
+			<div className="result">result</div>
 			<button className="panel-item ac">AC</button>
-			<button className="panel-item del">DEL</button>
-			<button className="panel-item div">÷</button>
+			<button className="panel-item ">DEL</button>
+			<button className="panel-item ">÷</button>
 
-			<button className="panel-item one">1</button>
-			<button className="panel-item two">2</button>
-			<button className="panel-item three">3</button>
-			<button className="panel-item mul">x</button>
+			<button className="panel-item ">1</button>
+			<button className="panel-item ">2</button>
+			<button className="panel-item ">3</button>
+			<button className="panel-item ">x</button>
 
-			<button className="panel-item four">4</button>
-			<button className="panel-item five">5</button>
-			<button className="panel-item six">6</button>
-			<button className="panel-item add">+</button>
+			<button className="panel-item ">4</button>
+			<button className="panel-item ">5</button>
+			<button className="panel-item ">6</button>
+			<button className="panel-item ">+</button>
 
-			<button className="panel-item seven">7</button>
-			<button className="panel-item eight">8</button>
-			<button className="panel-item nine">9</button>
-			<button className="panel-item minus">-</button>
+			<button className="panel-item ">7</button>
+			<button className="panel-item ">8</button>
+			<button className="panel-item ">9</button>
+			<button className="panel-item ">-</button>
 
-			<button className="panel-item dot">.</button>
-			<button className="panel-item zero">0</button>
+			<button className="panel-item ">.</button>
+			<button className="panel-item ">0</button>
 			<button className="panel-item equal">=</button>
 		</div>
 	);
