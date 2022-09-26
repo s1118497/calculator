16e9824657a124eda573ab30360ffc73093a3294
ccksam <chanyuksingsam@gmail.com>
2022-09-26 09:55:38 +0800

    save operands as string

--- src/components/Panel/index.jsx
+++ src/components/Panel/index.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 
 export default function Panel() {
 	// State 1: Array to store operands, need 2 to start calculation, need only 1 if clear/del
-	const [operandsArr, setOperand] = useState([]);
+	const [operandsArr, setOperands] = useState([]);
 
 	// State 2: Operator to store what operation to do
 	// +: add || -: minus || x: mul || ÷: div
@@ -15,58 +15,37 @@ export default function Panel() {
 	// State 4: Flags for presence of operands [1st operand, 2nd operand ...]
 	let [flags, setFlags] = useState([0, 0]);
 
-	// // State 5: Flag for decimal
-	// let [decimal, setDecimal] = useState(false);
 	/* ------------------------------------------------------------------------- */
+
 	//  callback for operands number input, store operands
 	const handleNumClick = () => (e) => {
+		if (display.length >= 13) return;
 		const text = e.target.innerText;
-		const value = text * 1;
 		// Condition 1: No operands
 		if (operandsArr.length === 0) {
-			// Condition 1.1: no operator ABSENT
-			if (!operator) {
-				setDisplay(value.toString());
-				setOperand([value]);
+			if (operator) {
+				setDisplay(text);
+				setOperands([0, text]);
 				setFlags([1, 0]);
-			}
-			// Condition 1.2:  operator PRESENT
-			else {
-				setDisplay(value.toString());
-				setOperand([0, value]);
-				setFlags([1, 1]);
+			} else {
+				setDisplay(text);
+				setOperands([text]);
+				setFlags([1, 0]);
 			}
 		}
 		// Condition 2: One operand
 		if (operandsArr.length === 1) {
 			// Condition 2.1: no operator
 			if (!operator) {
-				let newText = display
-					? display + text
-					: display === "0"
-					? operandsArr[0] + text
-					: text;
-				let newValue = newText * 1;
-				setDisplay(newValue.toString());
-				setOperand([newValue]);
+				let newText = display === "0" ? text : display + text;
+				setDisplay(newText);
+				setOperands([newText]);
 			}
-
 			// Condition 2.2: has operator
 			else {
-				// Condition 2.2.1: number > operator > number --> accept 2nd number
-				if (flags[0]) {
-					setDisplay(value.toString());
-					setOperand([operandsArr[0], value]);
-					setFlags([1, 1]);
-				}
-				// Condition 2.2.1:   operator > number > number --> update 1st number
-				else {
-					let newText = operandsArr[0] + text;
-					// for 1 exception: 0 start, display will be 01213, convert to number to avoid
-					let newValue = newText * 1;
-					setDisplay(newValue.toString());
-					setOperand([newValue]);
-				}
+				setDisplay(text);
+				setOperands([operandsArr[0], text]);
+				setFlags([1, 1]);
 			}
 		}
 		// Condition 3: Two operands (must have operator)
@@ -74,79 +53,79 @@ export default function Panel() {
 			// Condition 3.1: flags[1] = false (only autofill second when operator present)
 			if (!flags[1]) {
 				setDisplay(text);
-				setOperand([operandsArr[0], value]);
+				setOperands([operandsArr[0], text]);
 				setFlags([1, 1]);
 			}
 			// Condition 3.2: flags[1] = true (user input second operand)
 			else {
-				let newText = operandsArr[1] + text;
-				let newValue = newText * 1;
-				// for 1 exception: 0 start, display will be 01213, convert to number to avoid
-				setDisplay(newValue.toString());
-				setOperand([operandsArr[0], newValue]);
-				setFlags([1, 1]);
+				let newText = display + text;
+				setDisplay(newText);
+				setOperands([operandsArr[0], newText]);
 			}
+		}
+		if (display === "Infinity") {
+			setDisplay(text);
+			setOperands([text]);
+			setFlags([1, 0]);
+			setOperator("");
 		}
 	};
 
 	// callback for operator click
 	function handleOpClick(type) {
-		// // Conditino 0: when no operand
-		// if (operandsArr.length === 0) setOperator(type);
-		// // Condition 1: when 1 operand, default 2nd = 1st operand
-		// if (operandsArr.length === 1) {
-		// 	// Condition 1.1: if operator present, execute with 0 then update operator
-		// 	if (operator) {
-		// 		setOperand([0, operandsArr[0]]);
-		// 		handleEqClick();
-		// 		setOperator(type);
-		// 	}
-		// 	// Condition 1.2: if  operator absent
-		// 	else {
-		// 		setOperator(type);
-		// 		setOperand([operandsArr[0], operandsArr[0]]);
-		// 		setFlags([1, 0]);
-		// 	}
-		// }
-		// // Condition 2: when 2 operands (imply operator PRESENT), calculate then update operator
-		// if (operandsArr.length === 2) {
-		// 	handleEqClick();
-		// 	setOperator(type);
-		// }
-
 		if (!flags[0]) setOperator(type);
 		if (flags[0] && !flags[1]) {
-			setOperator(type);
-			setOperand([operandsArr[0], operandsArr[0]]);
+			// if (operandsArr[0] * 1 < 0 && type === "minus") setOperator("minus");
+			if (operator) {
+				// only for 負數 "+"
+				// for: - 6 + 9
+				if (type === "add") {
+					handleEqClick();
+					setOperator(type);
+				}
+			} else {
+				setOperator(type);
+				setOperands([operandsArr[0], operandsArr[0]]);
+			}
 		}
-		// case 1: operator > num > operator
-		// case 2: num > operator > num > operator
+		// Only support +/-: 4 + 3 -/+ 2
+		// Not support x/÷ : 4 - 3 * 2 = -2, beacuse it needs 3緩存
 		if (flags[0] && flags[1]) {
 			if (operator) {
-				// First calculate
-				handleEqClick();
-				// Then update operator
+				handleEqClick("op");
 				setOperator(type);
+				setFlags([1, 0]);
 			}
 		}
 	}
 
 	//  callback for "=", calculation
-	function handleEqClick() {
+	function handleEqClick(op) {
 		// When NO operands / Infinity
-		if (!operandsArr.length || operandsArr.includes("Infinity")) return;
+		if (!operandsArr.length || operandsArr.includes("Infinity") || !operator) return;
 		const map = {
-			add: () => operandsArr[0] + (operandsArr[1] ?? operandsArr[0]),
+			// string operation supported, but be aware of "+" is considered as string concat!
+			add: () =>
+				operandsArr.length === 2
+					? operandsArr[0] * 1 + (operandsArr[1] * 1 ?? operandsArr[0] * 1)
+					: operandsArr[0] * 1,
 			minus: () => operandsArr[0] - (operandsArr[1] ?? operandsArr[0]),
-			mul: () => operandsArr[0] * (operandsArr[1] ?? operandsArr[0]),
-			div: () => operandsArr[0] / (operandsArr[1] ?? operandsArr[0]),
+			mul: () =>
+				operandsArr.length === 2 ? operandsArr[0] * (operandsArr[1] ?? operandsArr[0]) : 0,
+			div: () => {
+				let quotient = operandsArr[0] / (operandsArr[1] ?? operandsArr[0]);
+				// for exception (0/0):  - > 3 > = > = > NaN
+				if (operandsArr.length === 2) {
+					if (isNaN(quotient)) return "Infinity";
+					else return quotient;
+				}
+			},
 		};
 		const operation = map[operator];
 		if (operation) {
 			let result = operation();
-			setDisplay(result.toString());
-			setOperand([result, operandsArr[1]]);
-			// setOperator("");
+			setDisplay(result.toString().slice(0, 14));
+			op ? setOperands([result, result]) : setOperands([result, operandsArr[1]]);
 			setFlags([1, 0]);
 		}
 	}
@@ -154,7 +133,7 @@ export default function Panel() {
 	//  callback for "AC", clear all state
 	function clearAll() {
 		return () => {
-			setOperand([]);
+			setOperands([]);
 			setDisplay("");
 			setOperator("");
 			setFlags([0, 0]);
@@ -173,33 +152,58 @@ export default function Panel() {
 				setOperator("");
 				// ("" * 1) => 0 ; parseInt("")=> NaN
 				if (updateText) {
-					setOperand([updateText * 1]);
+					setOperands([updateText]);
 				} else {
-					setOperand([]);
+					setOperands([]);
 					setFlags([0, 0]);
 				}
 			}
 			// when 2 operands
 			else if (updateText) {
-				setOperand([operandsArr[0], updateText * 1]);
+				setOperands([operandsArr[0], updateText]);
 			} else {
-				setOperand([operandsArr[0], operandsArr[0]]);
+				setOperands([operandsArr[0], operandsArr[0]]);
 				setFlags([1, 0]);
 			}
 		};
 	}
 
 	// callback for ".", handle decimal value
-	function handleDeciClick() {
+	function handleDeCiClick() {
 		return () => {
-			if (display.includes(".")) return;
-			setDisplay((oldDisplay) => oldDisplay + ".");
+			if (operandsArr.length === 2) {
+				if (flags[1]) {
+					if (display.includes(".")) return;
+					else {
+						let newText = display + ".";
+						setDisplay(newText);
+						setOperands([operandsArr[0], newText]);
+						setFlags([1, 1]);
+					}
+				} else if (!flags[1]) {
+					let newText = "0.";
+					setDisplay(newText);
+					setOperands([operandsArr[0], newText]);
+					setFlags([1, 1]);
+				}
+			}
+
+			if (operandsArr.length <= 1) {
+				if (display.includes(".")) return;
+				else {
+					let newText = display ? display + "." : "0.";
+					setDisplay(newText);
+					setOperands([newText]);
+					setFlags([1, 0]);
+				}
+			}
 		};
 	}
 
 	return (
 		<div className="panel-container">
 			<div className="result">{display ? display : "0"}</div>
+			{/* <div className="result">12314323551513516136363</div> */}
 			<button className="panel-item ac" onClick={clearAll()}>
 				AC
 			</button>
@@ -249,7 +253,7 @@ export default function Panel() {
 				-
 			</button>
 
-			<button className="panel-item " onClick={handleDeciClick()}>
+			<button className="panel-item " onClick={handleDeCiClick()}>
 				.
 			</button>
 			<button className="panel-item " onClick={handleNumClick()}>
